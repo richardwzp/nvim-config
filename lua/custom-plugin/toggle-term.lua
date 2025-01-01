96bb85d552328379289646ef063259e99cc4bbb3
richardwzp <wang.zipeng@northeastern.edu>
2025-01-01 03:53:18 -0500

    add terminal toggle

---
+++ lua/custom-plugin/toggle-term.lua
@@ -0,0 +1,17 @@
+return {
+  'akinsho/toggleterm.nvim',
+  config = function()
+    require('toggleterm').setup {}
+    vim.keymap.set('n', '\\=', function()
+      local term = require 'toggleterm.terminal'
+      local terminal = term.get_or_create_term(23)
+      if terminal:is_open() then
+        terminal:close()
+      else
+        terminal:open()
+        terminal:set_mode 'i'
+      end
+    end)
+    vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { noremap = true })
+  end,
+}
