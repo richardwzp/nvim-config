1d9af31ba5e95d1e2d376f929ac0e6d9023050a9
richardwzp <wang.zipeng@northeastern.edu>
2025-01-02 23:10:35 -0500

    fix length for fidget

--- lua/custom-plugin/custom-lsp/fidget.lua
+++ lua/custom-plugin/custom-lsp/fidget.lua
@@ -7,6 +7,13 @@ return {
         window = {
           border = 'rounded',
           winblend = 50,
+          max_width = 0, -- Maximum width of the notification window
+          max_height = 5, -- Maximum height of the notification window
+        },
+      },
+      integration = {
+        ['nvim-tree'] = {
+          enable = true, -- Integrate with nvim-tree/nvim-tree.lua (if installed)
         },
       },
     }
