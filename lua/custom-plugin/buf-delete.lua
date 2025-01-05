1c04de68ddb090d49cd872c5a700876092e54d9c
richardwzp <wang.zipeng@northeastern.edu>
2025-01-04 20:53:25 -0500

    now split window will be deleted properly, and nvim-tree is recognized as not an open buffer for quiting

--- lua/custom-plugin/buf-delete.lua
+++ lua/custom-plugin/buf-delete.lua
@@ -1,5 +1,17 @@
 local quit_buffer_gracefully = function()
-  require('bufdelete').bufdelete(0, true)
+  local buf_delete = require 'bufdelete'
+
+  buf_delete.bufdelete(0, true)
+  -- if there's more than one window, that means there's split going on. simply close it.
+  local win_count = #vim.api.nvim_tabpage_list_wins(0)
+  local suc, res = pcall(require, 'nvim-tree.api')
+  -- don't count nvim tree extension
+  if suc and res.tree.is_visible() then
+    win_count = win_count - 1
+  end
+  if win_count > 1 then
+    vim.api.nvim_win_hide(0)
+  end
 end
 
 return {
