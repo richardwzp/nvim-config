ee68d748b3bb586143dcc1fc923c3ba254c24e87
richardwzp <wang.zipeng@northeastern.edu>
2025-01-08 21:03:38 -0800

    correct select keybind, set min filecount to 1 for persistence

--- lua/custom-plugin/session-persistence.lua
+++ lua/custom-plugin/session-persistence.lua
@@ -5,7 +5,7 @@ return {
     dir = vim.fn.stdpath 'state' .. '/sessions/', -- directory where session files are saved
     -- minimum number of file buffers that need to be open to save
     -- Set to 0 to always save
-    need = 0,
+    need = 1,
     -- branch = true, -- use git branch to save session
   },
   keys = {
@@ -33,7 +33,7 @@ return {
     {
       '<leader>ps',
       function()
-        require('persistence').stop()
+        require('persistence').select()
       end,
       desc = 'Select [P]ersisted [S]ession',
     },
